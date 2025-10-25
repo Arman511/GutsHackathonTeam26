f7b09d02faac3c78c01b5e475162c52402232b67
himanipatney <himanipatney197@gmail.com>
2025-10-25 20:19:37 +0100

    endpoints yay

--- backend/app.py
+++ backend/app.py
@@ -6,12 +6,16 @@ from sqlalchemy.orm import Session
 from backend import models
 from backend.auth import get_current_user, auth_router
 from backend.events import event_router
+from backend.locations import location_router
+from backend.result import result_router
 from backend.database import SessionLocal, engine
 from backend.dependencies import db_dependency as db_dependency
 
 app = FastAPI()
 app.include_router(auth_router, prefix="/api")
 app.include_router(event_router, prefix="/api")
+app.include_router(location_router, prefix="/api")
+app.include_router(result_router, prefix="/api")
 models.Base.metadata.create_all(bind=engine)
 
 user_dependency = Annotated[dict, Depends(get_current_user)]
--- backend/models.py
+++ backend/models.py
@@ -19,6 +19,57 @@ class CreateUserRequest(BaseModel):
     username: str
     password: str
 
+class CreateEventRequest(BaseModel):
+    event_name: str
+    event_date: str
+    location: int
+    description: str
+    price_range: str
+    outdoor: bool
+    group_activity: bool
+    vegetarian_options: bool
+    drinks: bool
+    food_available: bool
+    accessible: bool
+    formal_attire: bool
+
+
+class CreateLocationRequest(BaseModel):
+    location: str
+    description: str
+    address: str
+    google_rating: int
+    price_range: str
+    outdoor: bool
+    group_activity: bool
+    vegetarian_options: bool
+    drinks: bool
+    food_available: bool
+    accessible: bool
+    formal_attire: bool
+    reservation_needed: bool
+    image_url: str
+
+class LocationSearchRequest(BaseModel):
+    keywords: list[str]
+    location: str | None = None
+    description: str | None = None
+    address: str | None = None
+    google_rating: int | None = None
+    price_range: str | None = None
+    outdoor: bool | None = None
+    group_activity: bool | None = None
+    vegetarian_options: bool | None = None
+    drinks: bool | None = None
+    food_available: bool | None = None
+    accessible: bool | None = None
+    formal_attire: bool | None = None
+    reservation_needed: bool | None = None
+    image_url: str | None = None
+
+class LocationRankingRequest(BaseModel):
+    location_id: int
+    ranking: int
 
 class Token(BaseModel):
     access_token: str
@@ -29,7 +80,7 @@ class Token(BaseModel):
 class LocationInfo(Base):
     __tablename__ = "LocationInfo"
     id = Column(Integer, primary_key=True, index=True)
-    location = Column(String)
+    location = Column(String, unique=True)
     description = Column(String)
     address = Column(String)
     reviews = relationship("ReviewData", back_populates="location")
@@ -43,7 +94,18 @@ class LocationInfo(Base):
     accessible = Column(Boolean)
     formal_attire = Column(Boolean)
     reservation_needed = Column(Boolean)
+    image_url = Column(String)
 
+class Keywords(Base):
+    __tablename__ = "Keywords"
+    id = Column(Integer, primary_key=True, index=True)
+    keyword = Column(String)
+
+class LocationKeywords(Base):
+    __tablename__ = "LocationKeywords"
+    id = Column(Integer, primary_key=True, index=True)
+    location_id = mapped_column(ForeignKey("LocationInfo.id"))
+    keyword_id = mapped_column(ForeignKey("Keywords.id"))
 
 class ReviewData(Base):
     __tablename__ = "ReviewData"
@@ -58,12 +120,24 @@ class ReviewData(Base):
 class EventsInfo(Base):
     __tablename__ = "EventsInfo"
     id = Column(Integer, primary_key=True, index=True)
+    user_id = mapped_column(ForeignKey("Users.id"))
     event_name = Column(String)
     event_date = Column(String)
-    location = relationship("LocationInfo")
-    location_id = mapped_column(ForeignKey("LocationInfo.id"))
+    location = mapped_column(ForeignKey("LocationInfo.id"))
     description = Column(String)
     attendees = relationship("EventUsers", back_populates="event")
+
+class EventConfigurations(Base):
+    __tablename__ = "EventConfigurations"
+    id = Column(Integer, primary_key=True, index=True)
+    event_id = mapped_column(ForeignKey("EventsInfo.id"))
+    price_range = Column(String)
+    group_activity = Column(Boolean)
+    vegetarian_options = Column(Boolean)
+    drinks = Column(Boolean)
+    food_available = Column(Boolean)
+    accessible = Column(Boolean)
+    formal_attire = Column(Boolean)
 
 class EventUsers(Base):
     __tablename__ = "EventUsers"
